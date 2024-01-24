90e52f88087190a30caf4d8bd2def3e4d4fa66e2
Staszek Cieslicki <stahoo@poczta.onet.pl>
2024-01-24 18:31:22 +0100

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import {Header} from "./header";
 import {Footer} from "../components/footer";
+import {Analytics} from "../components/analytics";
 
 const inter = Inter({ subsets: ['latin'] })
 
