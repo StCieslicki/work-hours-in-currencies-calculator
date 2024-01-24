5afd4d1e9519d8bb0872e3405334b5737abaf717
Staszek Cieslicki <stahoo@poczta.onet.pl>
2024-01-24 17:59:55 +0100

    better design

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -3,36 +3,49 @@ import {useEffect, useState} from "react";
 import {ErrorMessage} from "../../components/error";
 import {Title} from "../../components/title";
 import {getFreeDaysCount, getWorkingDays, getYearDaysCount} from "../../utils/holidays";
-import {getEuroRate, getUsdRate} from "../../utils/currencies";
+import {Currencies } from "../../utils/currencies";
 import {Row} from "../../components/row";
 import {Input} from "../../components/input";
 
 export default function Home() {
-    const nonVatRate = 1;
-    const vatRate = 1.23;
-    const defaultHourRate = 120;
-    const fixed = 0;
-
     const currentYear = new Date().getFullYear();
+
+    const defaults = {
+        nonVatRate: 1,
+        vatRate: 1.23,
+        defaultHourRate: 120,
+        defaultHourPerDay: 8,
+
+        defaltAvgDayPerMonth: 22,
+        daysInYear: getYearDaysCount(currentYear),
+        workingDays: getWorkingDays(currentYear),
+        freeDays: getFreeDaysCount(currentYear),
+        defaultPayedLeaveDays: 0,
+
+        fixedToForLarge: 0,
+        fixedToForSmall: 2,
+    }
 
     const [error, setError] = useState("");
 
     const [currencyDate, setCurrencyDate] = useState();
+    const [plnRate, setPlnRate] = useState(1);
     const [usdRate, setUsdRate] = useState();
     const [euroRate, setEuroRate] = useState();
 
-    const [hourPerDay, setHourPerDay] = useState(8);
+    const [hourPerDay, setHourPerDay] = useState(defaults.defaultHourPerDay);
 
-    const [nettoBruttoRate, setNettoBruttoRate] = useState(nonVatRate);
+    const [nettoBruttoRate, setNettoBruttoRate] = useState(defaults.nonVatRate);
 
     const [year, setYear] = useState(currentYear);
-    const [daysInYear, setDaysInYear] = useState(getYearDaysCount(currentYear));
-    const [workingDays, setWorkingDays] = useState(getWorkingDays(currentYear));
-    const [freeDays, setFreeDays] = useState(getFreeDaysCount(currentYear));
+    const [daysInYear, setDaysInYear] = useState(defaults.daysInYear);
+    const [workingDays, setWorkingDays] = useState(defaults.workingDays);
+    const [freeDays, setFreeDays] = useState(defaults.freeDays);
+    const [payedLeaveDays, setPayedLeaveDays] = useState(defaults.defaultPayedLeaveDays);
 
-    const [avgDayPerMonth, setAvgDayPerMonth] = useState(22);
+    const [avgDayPerMonth, setAvgDayPerMonth] = useState(defaults.defaltAvgDayPerMonth);
 
-    const [hourRatePln, setHourRatePln] = useState(defaultHourRate);
+    const [hourRatePln, setHourRatePln] = useState(defaults.defaultHourRate);
     const [hourRateUsd, setHourRateUsd] = useState();
     const [hourRateEur, setHourRateEur] = useState();
 
@@ -44,29 +57,38 @@ export default function Home() {
     const [monthlyIncomeUsd, setMonthlyIncomeUsd] = useState();
     const [monthlyIncomeEur, setMonthlyIncomeEur] = useState();
 
+    function recalculateCurrenciesByPln(pln, rates) {
+        const usd = (pln / rates.usd).toFixed(defaults.fixedToForSmall);
+        const eur = (pln / rates.eur).toFixed(defaults.fixedToForSmall);
+
+        return {pln, usd, eur}
+    }
+
     useEffect( () => {
         (async () => {
-            const usdRate = await getUsdRate();
+            const currencies = await new Currencies();
+
+            const usdRate = currencies.getUsdRate();
             setCurrencyDate(usdRate.date);
             setUsdRate(usdRate.rate);
 
-            const pln = defaultHourRate;
-            const usd = Number((pln / usdRate.rate).toFixed(2));
+            const eurRate = currencies.getEurRate();
+            setEuroRate(eurRate.rate);
+
+            const rates = {usd: usdRate.rate, eur: usdRate.rate};
+
+            const pln = defaults.defaultHourRate;
+
+            const currenciesObj = recalculateCurrenciesByPln(pln, rates);
+
+            const {usd, eur} = currenciesObj;
 
             setHourRateUsd(usd);
-
-            recalculateByHourRate({pln, usd});
-        })();
-
-        (async() => {
-            const euroRate = await getEuroRate();
-            setEuroRate(euroRate.rate);
-
-            const pln = defaultHourRate;
-            const eur = Number((pln / euroRate.rate).toFixed(2));
             setHourRateEur(eur);
 
-            recalculateByHourRate({pln, eur});
+            recalculateByHourRate(currenciesObj);
+            recalculateByYearlyIncome(currenciesObj);
+            recalculateByMonthlyIncome(currenciesObj);
         })();
         }, []);
 
@@ -81,7 +103,7 @@ export default function Home() {
     }, [year]);
 
     useEffect(() => {
-        setAvgDayPerMonth(Number((workingDays / 12).toFixed(2)));
+        setAvgDayPerMonth(Number((workingDays / 12).toFixed(defaults.fixedToForSmall)));
     }, [workingDays]);
 
     useEffect(() => {
@@ -90,6 +112,11 @@ export default function Home() {
         recalculateByMonthlyIncome({pln: hourRatePln, usd: hourRateUsd, eur: hourRateEur});
     }, [hourPerDay]);
 
+    useEffect(()=> {
+        // recalculateByHourRate({pln: hourRatePln, usd: hourRateUsd, eur: hourRateEur});
+        recalculateByYearlyIncome({pln: hourRatePln, usd: hourRateUsd, eur: hourRateEur});
+        recalculateByMonthlyIncome({pln: hourRatePln, usd: hourRateUsd, eur: hourRateEur});
+    }, [payedLeaveDays])
 
     const onSubmit = async () => {
         setError("");
@@ -97,82 +124,122 @@ export default function Home() {
         console.log('calculate');
     };
 
-    const recalculateByYearlyIncome = ({pln, usd, eur}) => {
-        setYearlyIncomePln(Number(pln * hourPerDay * workingDays).toFixed(fixed));
+    const recalculateByHourRate = (({pln, usd, eur}) => {
+        setHourRatePln(pln);
+        setHourRateUsd(usd);
+        setHourRateEur(eur);
+    });
 
-        if (usd) {
-            setYearlyIncomeUsd(Number(usd * hourPerDay * workingDays).toFixed(fixed));
-        }
+    const recalculateByHourlyRatePln = (value) => {
+        const pln = value;
+        const usd = (value * plnRate / usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = (value * plnRate / euroRate).toFixed(defaults.fixedToForSmall);
 
-        if (eur) {
-            setYearlyIncomeEur(Number(eur * hourPerDay * workingDays).toFixed(fixed));
-        }
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByYearlyIncome({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
     }
+
+    const recalculateByHourlyRateUsd = (value) => {
+        const pln = (value * usdRate / plnRate).toFixed(defaults.fixedToForSmall);
+        const usd = value;
+        const eur = (value * usdRate / euroRate).toFixed(defaults.fixedToForSmall);
+
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByYearlyIncome({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
+    }
+
+    const recalculateByHourlyRateEur = (value) => {
+        const pln = (value * euroRate).toFixed(defaults.fixedToForSmall);
+        const usd = (value * euroRate / usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = value;
+
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByYearlyIncome({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
+    }
+
+
 
     const recalculateByMonthlyIncome = ({pln, usd, eur}) => {
-        setMonthlyIncomePln(Number(pln * hourPerDay * workingDays / 12).toFixed(fixed));
-
-        if (usd) {
-            setMonthlyIncomeUsd(Number(usd * hourPerDay * workingDays / 12).toFixed(fixed));
-        }
-
-        if (eur) {
-            setMonthlyIncomeEur(Number(eur * hourPerDay * workingDays / 12).toFixed(fixed));
-        }
-    }
-
-    const recalculateByHourRate = (({pln, usd, eur}) => {
-        if (pln) {
-            setHourRatePln(pln);
-        }
-
-        if (usd) {
-            setHourRateUsd(usd);
-        }
-
-        if (eur) {
-            setHourRateEur(eur);
-        }
-
-        // recalculateByYearlyIncome({pln, usd, eur});
-
-        // recalculateByMonthlyIncome({pln, usd, eur});
-    })
-
-    const recalculateByHourlyRatePln = (pln) => {
-        const usd = Number((pln / usdRate).toFixed(2));
-        const eur = Number((pln / euroRate).toFixed(2))
-
-        recalculateByHourRate({pln, usd, eur});
-        recalculateByYearlyIncome({pln, usd, eur});
-        recalculateByMonthlyIncome({pln, usd, eur});
-    }
-
-    const recalculateByHourlyRateUsd = (usd) => {
-        const pln = Number((usd * usdRate).toFixed(2));
-        const eur = Number((usd * usdRate / euroRate).toFixed(2));
-
-        recalculateByHourRate({pln, usd, eur});
-        recalculateByYearlyIncome({pln, usd, eur});
-        recalculateByMonthlyIncome({pln, usd, eur});
-    }
-
-    const recalculateByHourlyRateEur = (eur) => {
-        const pln = Number((eur * euroRate).toFixed(2));
-        const usd = Number((eur * euroRate / usdRate).toFixed(2));
-
-        recalculateByHourRate({pln, usd, eur});
-        recalculateByYearlyIncome({pln, usd, eur});
-        recalculateByMonthlyIncome({pln, usd, eur});
+        pln ? setMonthlyIncomePln((pln * hourPerDay * (workingDays - payedLeaveDays) / 12).toFixed(defaults.fixedToForLarge)) : null;
+        usd ? setMonthlyIncomeUsd((usd * hourPerDay * (workingDays - payedLeaveDays) / 12).toFixed(defaults.fixedToForLarge)) : null;
+        eur ? setMonthlyIncomeEur((eur * hourPerDay * (workingDays - payedLeaveDays) / 12).toFixed(defaults.fixedToForLarge)) : null;
     }
 
     const recalculateByMonthlyIncomePln = (monthlyPln) => {
-        // setMonthlyIncomePln(Number(pln * hourPerDay * workingDays / 12).toFixed(fixed));
-        const pln = (monthlyPln / hourPerDay / (workingDays * 12)).toFixed(2);
-        const usd = pln * usdRate;
-        const eur = pln * euroRate;
+        const pln = (monthlyPln / hourPerDay / ((workingDays - payedLeaveDays) / 12)).toFixed(defaults.fixedToForSmall);
+        const usd = (pln / usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = (pln / euroRate).toFixed(defaults.fixedToForSmall);
 
+        setMonthlyIncomePln(monthlyPln);
         recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({usd, eur});
+        recalculateByYearlyIncome({pln, usd, eur});
+    }
+
+    const recalculateByMonthlyIncomeUsd = (monthlyUsd) => {
+        const usd = (monthlyUsd / hourPerDay / ((workingDays - payedLeaveDays) / 12)).toFixed(defaults.fixedToForSmall);
+        const pln = (usd * usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = (usd * usdRate / euroRate).toFixed(defaults.fixedToForSmall);
+
+        setMonthlyIncomeUsd(monthlyUsd);
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, eur});
+        recalculateByYearlyIncome({pln, usd, eur});
+    }
+
+    const recalculateByMonthlyIncomeEur = (monthlyEur) => {
+        const eur = (monthlyEur / hourPerDay / ((workingDays - payedLeaveDays) / 12)).toFixed(defaults.fixedToForSmall);
+        const pln = (eur * euroRate).toFixed(defaults.fixedToForSmall);
+        const usd = (eur * usdRate / euroRate).toFixed(defaults.fixedToForSmall);
+
+        setMonthlyIncomeEur(monthlyEur);
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd});
+        recalculateByYearlyIncome({pln, usd, eur});
+    }
+
+
+
+    const recalculateByYearlyIncome = ({pln, usd, eur}) => {
+        pln ? setYearlyIncomePln((pln * hourPerDay * (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForLarge)) : null;
+        usd ? setYearlyIncomeUsd((usd * hourPerDay * (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForLarge)) : null;
+        eur ? setYearlyIncomeEur((eur * hourPerDay * (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForLarge)) : null;
+    }
+
+    const recalculateByYearlyIncomePln = (YearlyPln) => {
+        const pln = (YearlyPln / hourPerDay / (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForSmall);
+        const usd = (pln / usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = (pln / euroRate).toFixed(defaults.fixedToForSmall);
+
+        setYearlyIncomePln(YearlyPln);
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
+        recalculateByYearlyIncome({usd, eur});
+    }
+
+    const recalculateByYearlyIncomeUsd = (yearlyUsd) => {
+        const usd = (yearlyUsd / hourPerDay / (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForSmall);
+        const pln = (usd * usdRate).toFixed(defaults.fixedToForSmall);
+        const eur = (usd * usdRate / euroRate).toFixed(defaults.fixedToForSmall);
+
+        setYearlyIncomeUsd(yearlyUsd);
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
+        recalculateByYearlyIncome({pln, eur});
+    }
+
+    const recalculateByYearlyIncomeEur = (yearlyEur) => {
+        const eur = (yearlyEur / hourPerDay / (workingDays - payedLeaveDays)).toFixed(defaults.fixedToForSmall);
+        const pln = (eur * euroRate).toFixed(defaults.fixedToForSmall);
+        const usd = (eur * usdRate / euroRate).toFixed(defaults.fixedToForSmall);
+
+        setYearlyIncomeEur(yearlyEur);
+        recalculateByHourRate({pln, usd, eur});
+        recalculateByMonthlyIncome({pln, usd, eur});
+        recalculateByYearlyIncome({pln, usd});
     }
 
     return (
@@ -185,9 +252,9 @@ export default function Home() {
                 }}
                 >
 
-                <Title>Currency Calculator</Title>
+                {/*<Title>Currency Calculator</Title>*/}
 
-                <Row>
+                <Row title="Settings" description={workingDays ? `There is ${workingDays} working days in ${year}, avg ${avgDayPerMonth} days per month` : ''}>
                     <Input
                         label="Year"
                         name="year"
@@ -201,7 +268,42 @@ export default function Home() {
                         value={hourPerDay}
                         onChange={setHourPerDay}
                     />
+
+                    <Input
+                        label="Payed leave days"
+                        name="payedLeaveDays"
+                        value={payedLeaveDays}
+                        onChange={setPayedLeaveDays}
+                        disabled
+                    />
                 </Row>
+
+                <Row title="Currencies" description={currencyDate ? `currencies from NBP (${currencyDate})` : ''}>
+                    <Input
+                        className="text-zink-400"
+                        label="PLN"
+                        name="pln"
+                        value={plnRate}
+                        disabled
+                    />
+
+                    <Input
+                        className="text-zink-400"
+                        label="USD"
+                        name="usd"
+                        value={usdRate}
+                        disabled
+                    />
+
+                    <Input
+                        className="text-zink-400"
+                        label="EUR"
+                        name="eur"
+                        value={euroRate}
+                        disabled
+                    />
+                </Row>
+
 
                 <Row title="Hour rate">
                     <Input
@@ -216,6 +318,7 @@ export default function Home() {
                         name="hourRateUsd"
                         value={hourRateUsd}
                         onChange={recalculateByHourlyRateUsd}
+                        disabled={!usdRate}
                     />
 
                     <Input
@@ -223,6 +326,7 @@ export default function Home() {
                         name="hourRateEur"
                         value={hourRateEur}
                         onChange={recalculateByHourlyRateEur}
+                        disabled={!euroRate}
                     />
                 </Row>
 
@@ -238,14 +342,16 @@ export default function Home() {
                         label="Monthly income USD"
                         name="monthlyIncomeUsd"
                         value={monthlyIncomeUsd}
-                        // onChange={recalculateByHourlyRateUsd}
+                        onChange={recalculateByMonthlyIncomeUsd}
+                        disabled={!usdRate}
                     />
 
                     <Input
                         label="Monthly income EUR"
                         name="monthlyIncomeEur"
                         value={monthlyIncomeEur}
-                        // onChange={recalculateByHourlyRateEur}
+                        onChange={recalculateByMonthlyIncomeEur}
+                        disabled={!euroRate}
                     />
                 </Row>
 
@@ -254,21 +360,23 @@ export default function Home() {
                         label="Yearly income PLN"
                         name="yearlyIncomePln"
                         value={yearlyIncomePln}
-                        // onChange={recalculateByHourlyRatePln}
+                        onChange={recalculateByYearlyIncomePln}
                     />
 
                     <Input
                         label="Yearly income USD"
                         name="yearlyIncomeUsd"
                         value={yearlyIncomeUsd}
-                        // onChange={recalculateByHourlyRateUsd}
+                        onChange={recalculateByYearlyIncomeUsd}
+                        disabled={!usdRate}
                     />
 
                     <Input
                         label="Yearly income EUR"
                         name="yearlyIncomeEur"
                         value={yearlyIncomeEur}
-                        // onChange={recalculateByHourlyRateEur}
+                        onChange={recalculateByYearlyIncomeEur}
+                        disabled={!euroRate}
                     />
                 </Row>
 
--- src/components/row.tsx
+++ src/components/row.tsx
@@ -1,11 +1,15 @@
 import {PropsWithChildren} from "react";
 
-export const Row: React.FC<PropsWithChildren> = ({ children, title }): JSX.Element => {
+export const Row: React.FC<PropsWithChildren> = ({ children, title, description }): JSX.Element => {
     return (
         <>
-        {title && <h2>{title}</h2>}
+        {/*{title && <h2 className="mt-5 mb-0 flex flex-col sm:items-center sm:justify-center sm:flex-row">{title} <span className="text-xs text-zinc-400">{description}</span></h2>}*/}
+        {title && <h2 className="mt-3 mb-0 flex flex-col justify-between items-center sm:flex-row">
+            <span className="mr-5">{title}</span>
+            <span className="text-xs text-zinc-400">{description}</span>
+        </h2>}
 
-        <div className="flex flex-col items-center justify-center w-full gap-4 mt-4 sm:flex-row">
+        <div className="flex flex-col items-center justify-center w-full gap-4 mt-1 sm:flex-row">
             {children}
         </div>
         </>
